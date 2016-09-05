8e1a4fdd8287427079c791dfce23c2f28b3a3bc2
jkjium <jkjium@gmail.com>
2016-09-05 18:05:47 -0500

    resi2bfactor

--- utils_protein.py
+++ utils_protein.py
@@ -3,16 +3,31 @@ get resid list of varname
 '''
 import sys
 from protein import protein
+from AAmap import AAmap
 
-def getseq():
+def resn2bfactor():
 	if len(sys.argv) < 3:
-		print 'getseq(): python utils_protein.py getseq pdbfile chainID'
+		print 'resn2bfactor(): replace b factor values with residue type.'
+		print 'resn2bfactor(): used for pymol spectrum b'
 		return
+	scoreValue = {
+							'X':0,'-': 0,'.': 0,'A': 1,'C': 2,'D': 3,'E': 4,'F': 5,'G': 6,'H': 7,'I': 8,'K': 9,
+							'L': 10,'M': 11,'N': 12,'P': 13,'Q': 14,'R': 15,'S': 16,'T': 17,'V': 18,'W': 19,'Y': 20, 'B': 3
+						}
+	aamap = AAmap()
 
 	pdbfile = sys.argv[2]
-	chain = sys.argv[3]
-	p = protein(pdbfile, chain)
-	print p.seq
+	p = protein(pdbfile)
+	outfile = '%s_rb.pdb' % pdbfile[:-4]
+	fout = open(outfile, 'w')
+	for a in p.atoms:
+		newBFactor = scoreValue[aamap.getAAmap(a.resName)]
+		print 'new b-factor: [%s : %s] -> %d' % (a.resName, aamap.getAAmap(a.resName), newBFactor)
+		a.tempFactor = newBFactor
+		fout.write(a.writeAtom())
+	fout.close()
+	print 'Output file: %s' % outfile
+
 
 def main():
 	if len(sys.argv)<3:
@@ -20,7 +35,7 @@ def main():
 		return
 
 	dispatch = {
-		'getseq': getseq
+		'resn2bfactor': resn2bfactor
 	}
 
 	cmd = sys.argv[1]
