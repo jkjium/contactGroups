489b6f7836ddb6efcce480b336a215520d013703
jkjium <jkjium@gmail.com>
2017-11-28 04:09:23 -0600

    1sthead

--- proc_1sthead.py
+++ proc_1sthead.py
@@ -1,12 +1,13 @@
 import sys
 
 def main():
-	if len(sys.argv) < 3:
-		print 'Usage: python proc_1sthead.py PF00000_full.txt ec|w'
+	if len(sys.argv) < 4:
+		print 'Usage: python proc_1sthead.py PF00000_full.txt ec|w cutoff'
 		exit()
 
 	msafile = sys.argv[1]
 	opt = sys.argv[2]
+	cutoff = sys.argv[3]
 	with open(msafile) as fp:
 		for line in fp:
 			strarr = line[1:].split('/')
@@ -14,7 +15,7 @@ def main():
 			if opt == 'ec':
 				print 'calculate_evolutionary_constraints(\'%s\',\'%s\',\'%s.dca\');' % (msafile, strarr[0], msafile)
 			else:
-				print 'evfold_weight(\'%s\',\'%s\', 0.3);' % (msafile, strarr[0])
+				print 'evfold_weight(\'%s\',\'%s\', %s);' % (msafile, strarr[0], cutoff)
 			break
 
 if __name__ == '__main__':
