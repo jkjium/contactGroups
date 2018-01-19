7081aaa9e1c40b7adfca17e438d567bd49e5c9f5
kjia <kjia@iastate.edu>
2018-01-19 14:44:09 -0600

    writegoset

--- utils_iprscan.py
+++ utils_iprscan.py
@@ -17,6 +17,13 @@ class iprscan(object):
 					continue
 				self.profile.append(line.split('\t'))
 
+
+	#Molecular Function: iron ion binding (GO:0005506)
+	#Biological Process: oxygen transport (GO:0015671)
+	def getgo(self, gostr):
+		return set([g.replace(',', '').strip() +')' for g in gostr.split(')') if len(g)!=0])
+		
+
 	def dump(self):
 		print '------------------------------------'
 		print self.name
@@ -27,18 +34,35 @@ class iprscan(object):
 	def goset(self):
 		goterms = set()
 		for p in self.profile:
-			print len(p)
 			if len(p) ==14:
-				goterms.update(set(p[13].split(',')))
-		print repr(goterms)
+				goterms.update(set([('%s,%s)\n' % (self.name, g.replace(',', '').strip())) for g in p[13].split(')') if len(g)!=0]))
+		return list(goterms)
 
 
+# write a file with name and goterms
+def writegoset(arglist):
+	if len(arglist) < 2:
+		cp._err('Usage: python utils_iprscan.py writegoset iprfile outfile')
+
+	iprfile = arglist[0]
+	outfile = arglist[1]
+
+	ipr = iprscan(iprfile)
+	gostr = ''.join(ipr.goset())
+	if len(gostr)!= 0:
+		with open(outfile,'w') as fp:
+			fp.write(gostr)
+		cp._info('save goterms to %s' % outfile)
+	else:
+		cp._info('empty goterms in %s' % iprfile)
 
 
 def foo(arglist):
 	ipr = iprscan(arglist[0])
 	ipr.dump()
-	ipr.goset()
+	goterms = ipr.goset()
+	for g in goterms:
+		print g
 
 
 if __name__ == '__main__':
@@ -47,6 +71,7 @@ if __name__ == '__main__':
 		cp._err('Usage: python utils_iprscan.py cmd [args ...]')
 
 	dispatch = {
+		'writegoset':	writegoset,
 		'foo': foo
 	}
 
