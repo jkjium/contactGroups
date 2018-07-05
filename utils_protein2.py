3706813cc8cc8f2723590983f835be03176be3d9
jkjium <jkjium@gmail.com>
2018-07-05 06:03:55 -0500

    protein2

--- utils_protein2.py
+++ utils_protein2.py
@@ -7,6 +7,44 @@ import numpy as np
 import commp as cp
 
 
+def ncgfreq(arglist):
+	if len(arglist)<3:
+		cp._err('Usage: python utils_protein2.py ncgfreq cglistfile bgfreqfile outfile')
+
+	cglistfile = arglist[0]
+	bgfreqfile = arglist[1]
+	outfile = arglist[2]
+
+	cgfreq = defaultdict(lambda:0)
+	with open(cglistfile) as fp:
+		for line in fp:
+			# 2 R 26 D
+			line = line.strip()
+			if len(line) == 0:
+				continue
+			sarr = line.split(' ')
+			key = '%s%s' % (sarr[1], sarr[3]) if sarr[1] <=sarr[3] else '%s%s' % (sarr[3], sarr[1])
+			cgfreq[key]+=1
+	#print repr(cgfreq)
+
+	bgfreq = np.loadtxt(bgfreqfile, delimiter=' ')
+	#print repr(bgfreq)
+	bgfreq = bgfreq/bgfreq.sum()
+	#print repr(bgfreq)
+
+	ncgfreq = []
+	for i in xrange(0, len(cp.aat01)):
+		for j in xrange(i, len(cp.aat01)):
+			key = '%s%s' % (cp.aat01[i], cp.aat01[j])
+			ncgfreq.append(cgfreq[key]/(bgfreq[i]*bgfreq[j]))
+			#print key,cgfreq[key],ncgfreq
+	nsum = sum(ncgfreq)
+	#print nsum
+
+	with open(outfile, 'w') as fout:
+		fout.write(' '.join(['%.4f' % (f/nsum) for f in ncgfreq]))
+	cp._info('save to %s' % outfile)
+
 def seqaafreq(arglist):
 	if len(arglist) < 2:
 		cp._err('Usage: python utils_protein2.py seqaafreq 1t3r.pdb all (output: 1t3r.pdb.all.saf)')
