56d0dc37ecc539e0e771012df3e0660b93372af6
jkjium <jkjium@gmail.com>
2019-09-19 11:24:51 -0500

    p53 extend ce

--- proc_mip2.py
+++ proc_mip2.py
@@ -4,39 +4,39 @@ import commp as cp
 
 def mi2dict(sdiifile):
 	ret = {}
+	stub=[]
+	listset=set()
 	with open(sdiifile) as fp:
 		for line in fp:
 			if len(line) < 2:
 				continue
 			kv = line.strip().split(' ')
 			ret['%s %s' % (kv[0], kv[1])] = float(kv[2])
-	return ret
+			listset.add(int(kv[0]))
+			listset.add(int(kv[1]))
+			stub.append('%s %s' % (kv[0], kv[1]))
+	colslist = list(listset)
+	colslist.sort()
+	return ret,stub,colslist
 
 def calc_MIp(arglist):
 	"""
-	calculate MIp based on .col file and raw sdii(MI)
-	for scsc pipeline 2.0
+	calculate MIp based on .rcol file and raw sdii(MI)
+	raw sdii format:
+	masi1 msai2 sdii
 	APC(a,b) = (MIax * MIbx) / avgMI
-
+	output: single column MIp corresponding to the original order in SDII file (for paste to append)
 	"""
-	if len(arglist) < 4:
+	if len(arglist) < 3:
 		print 'Usage: python proc_MIp.py colfile sdiifile outfile'
-		print 'example: python proc_mip.py PF03176_p90.txt.col PF03176_p90.txt.all_2_sdii PF03176_p90.mip'
-		print 'output: p1 p2 mi apc mip'
+		print 'example: python proc_mip2.py PF03176_p90.txt.all_2_sdii PF03176_p90.mip'
 		return
 
-	colfile = sys.argv[1]
-	sdiifile = sys.argv[2]
-	outfile = sys.argv[3]
+	sdiifile = sys.argv[1]
+	outfile = sys.argv[2]
 	#print colfile, sdiifile, outfile
 
-	cols = sorted([int(j) for j in np.loadtxt(colfile, delimiter=',')])
-	#print repr(cols), len(cols)
-	if len(cols) == 0:
-		cp._info('err:empty column file: %s' % colfile)
-		return
-
-	sdiidict = mi2dict(sdiifile)
+	sdiidict, stublist, cols = mi2dict(sdiifile)
 	#print sdiidict
 	if len(sdiidict) == 0:
 		cp._info('err:empty sdii file: %s' % sdiifile)
@@ -53,23 +53,28 @@ def calc_MIp(arglist):
 				continue
 			k = ('%s %s' % (cols[i], cols[j])) if i < j else ('%s %s' % (cols[j], cols[i]))
 			#print k, sdiidict[k]
-			marginMI+= sdiidict[k]
+			if k in sdiidict:
+				marginMI+= sdiidict[k]
 		#print marginMI, len(cols)-1
 		MIax[cols[i]] = marginMI / (len(cols)-1)
 	
 	#print repr(MIax)
-
+	MIpdict={}
 	MIp = 0.0
-	fout = open(outfile, 'w')
 	for p in xrange(0, len(cols)):
 		for q in xrange(p+1, len(cols)):
 			apc = (MIax[cols[p]] * MIax[cols[q]])/avgMI
 			k = '%d %d' % (cols[p], cols[q])
-			MIp = sdiidict[k] - apc
-			fout.write('%s %.8f %.8f %.8f\n' % (k, sdiidict[k], apc, MIp))
-	fout.close()
-	print 'save records to %s' % outfile
+			if k in sdiidict:
+				MIp = sdiidict[k] - apc
+				MIpdict[k] = MIp
+				print '%s %.8f %.8f %.8f\n' % (k, sdiidict[k], apc, MIp)
+			#fout.write('%s %.8f %.8f %.8f\n' % (k, sdiidict[k], apc, MIp))
 
+	with open(outfile, 'w') as fout:
+		for k in stublist:
+			fout.write('%.8f\n' % MIpdict[k])
+	print 'save records to %s' % outfile
 
 def main():
 	calc_MIp(sys.argv)
