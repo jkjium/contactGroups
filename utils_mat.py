b2bc21c36b7a5438cfb8920e61781cc8390b166c
U-IASTATE\bbmb_kjia_admin <bbmb_kjia_admin@BB-RJER-3630519.bb.iastate.edu>
2021-09-13 17:53:08 -0500

    matrix operation

---
+++ utils_mat.py
@@ -0,0 +1,37 @@
+import numpy as np
+import commp as cp
+
+def _fillbyticksep(mat, t, s, v):
+    n = len(t)
+    for i in range(n):
+        for j in range(i,n):
+            if t[j]-t[i] <= s:
+                mat[i,j] = mat[j,i] = v
+    return mat
+
+# thrsholding matrix by tick separations
+# for example filtering cemat by large sequential separations
+def fillbyticksep(args):
+    assert len(args) == 5, 'Usage:python utils_mat.py fillbyticksep inmatfile tickfile separation{int value} value outfile'
+
+    inmatfile = args[0]
+    tickfile = args[1]
+    s = int(args[2])
+    v = float(args[3])
+    outfile = args[4]
+
+    mat = np.loadtxt(inmatfile)
+    t = np.loadtxt(tickfile).astype(int)
+    n = len(t)
+    assert mat.shape[0] == n, 'matrix dimension and number of ticks not match.'
+
+    outmat = _fillbyticksep(mat, t, s, v)
+    np.savetxt(outfile, outmat, fmt='%.3f')
+    cp._info('save filtered matrix to %s' % outfile)
+
+
+def foo(args):
+    print(len(args))
+
+if __name__=='__main__':
+    cp.dispatch(__name__)
