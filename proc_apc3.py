7179b26101336246b0ffe1dbac193119fc2ad8ae
jkjium <jkjium@gmail.com>
2022-10-02 22:46:36 -0500

    rank vs ppv

--- proc_apc3.py
+++ proc_apc3.py
@@ -1,41 +1,47 @@
 import commp as cp
 import numpy as np
 
-# rank ce value as percentage or x * L
+# prepare data for plotting accumulative_accuracy_ratio vs ranking_ratio(top X percentage ranked)
 # extended from utils_ps3.py appendrankp
-# input: .vec15
+# input: ce value file(.vec15), ce value indeces {1,2,3},  response_index, response_cutoff
 # output: .png
-def cerankl(args):
-    assert len(args) == 4, 'Usage: python utils_ce.py cerankl cefile collist{1,2,3,} L outfile'
+def rankp(args):
+    assert len(args) == 6, 'Usage: python utils_ce.py cerankl cefile collist{1,2,3,} yi yv legends outfile'
     from scipy.stats import rankdata
     import matplotlib.pyplot as plt
 
     infile = args[0]
     cols = [int(c) for c in args[1].split(',')]
-    L = float(args[2])
-    outfile = args[3]
+    yi = int(args[2])
+    yv = float(args[3])
+    legends = args[4]
+    outfile = args[5]
 
     #.vec15: r3.md.tc3.sdii3.ii3.dtc3
-    cp._info('loading .vec15 data ...')
+    cp._info('loading data ...')
     data = np.loadtxt(infile)
-    n = len(data)
+    n = len(data) # number of pairs/triplets
     outlist = []
     for i in cols:
         # get rank of ith ce
         cp._info('ranking of %dth cevalue' % i)
-        p = rankdata(-data[:,i], method='min')
-        # r: 01 rank, .vec15[:,3] is mdist
-        r =np.c_[(data[:,3]<5).astype(float), p]
-        # sort by rank
-        # s: 01 sorted_rank
-        cp._info('sorting ranks ...')
+        p = rankdata(-data[:,i], method='min') # get rank value for ith ce column
+
+        # generating 0,1 response then paste with rank values
+        # r: 01 rank, .vec15[:,3] is mdist (response y)
+        r =np.c_[(data[:,yi]<yv).astype(float), p]
+
+        # sort vec2 by rank values
+        # s: vec2: 01, sorted_rank
+        cp._info('sorting by ranks ...')
         s = r[r[:,1].argsort()]
-        total_contact = sum(r[:,0])
+
+        total_contact = sum(r[:,0]) # sum of 1s
         cp._info('total contact: %d' % total_contact)
         
-        # sL: 01, sorted_rank/L
+        # sL: 01, rank ratio (high to low order)
         #sL = np.c_[s[:,0], s[:,1]/L]
-        sL = np.c_[s[:,0], s[:,1]/n]
+        sL = np.c_[s[:,0], s[:,1]/n] # sL[:,1] in [1/n, 1]
         # masked sorted rank/L
         # msL: 01 sorted_rank/L
         #msL = sL[sL[:,1] <=10.0, :]
@@ -44,26 +50,23 @@ def cerankl(args):
         # calculate accumulative number of contact
         cp._info('calculating accumulative contact numbers')
         a = np.cumsum(msL[:,0])
-        #a = [sum(s[0:j+1,0]) for j in range(len(msL))]
 
-        # outplt: sorted_rank/L acc
+        # outplt: x-axis: sorted_rank_ratio y-axis:accumulative_accuracy_ratio
         outplt = np.c_[msL[:,1], a/total_contact]
-        # ppv = acc[:,2] / acc[:,1]
+
+        # save to ce separate files 
 
         # ppv, sorted_rank
-        #outplt = np.c_[ppv, acc[:,1]/L]
-        #print(outplt)
         cp._info('plotting rank/L vs acc')
         plt.plot(outplt[:,0], outplt[:,1])
-        plt.tight_layout()
-        plt.show()
 
-    '''
-        
-    outnplist = np.array(outlist)
-    np.savetxt(outfile, outnplist.T, fmt='%.6f')
-    cp._info('save L/x rank to %s' % outfile)
-    '''
+    # finishing plots
+    plt.xlabel('ranking ratio')
+    plt.ylabel('ppv')
+    plt.legend(loc='best')
+    plt.savefig(outfile)    
+    plt.tight_layout()
+    plt.show()
 
 if __name__=='__main__':
     cp.dispatch(__name__)
