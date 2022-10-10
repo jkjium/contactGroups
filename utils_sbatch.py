bd707c8455cd5bad66cb465ac6cfdb8950984494
U-IASTATE\bbmb_kjia_admin <bbmb_kjia_admin@BB-RJER-3630519.bb.iastate.edu>
2022-10-10 12:03:52 -0500

    sbatch functions

--- utils_sbatch.py
+++ utils_sbatch.py
@@ -2,13 +2,36 @@ import commp as cp
 import numpy as np
 import sys
 
-
-def _sbatchstr(name, cmd, hours, partition):
+def _af2str(name, cmd, hours, partition=None):
 	sbatch=[]
 	sbatch.append('#!/bin/bash')
 	sbatch.append('#SBATCH --nodes=1')
 	sbatch.append('#SBATCH --cpus-per-task=8')
-	sbatch.append('#SBATCH --mem=64G')
+	sbatch.append('#SBATCH --mem=128G')
+	
+	sbatch.append('\n#SBATCH --time=%d:0:0\n' % hours)
+	print('%s %d' % (name, hours))
+
+	sbatch.append('#SBATCH --output=job.%s.out' % name)
+	sbatch.append('#SBATCH --error=job.%s.err\n' % name)
+
+	sbatch.append('\n#SBATCH --partition=gpu') 
+	sbatch.append('#SBATCH --gres=gpu:1\n') 
+
+	# module
+	sbatch.append('module load alphafold\n')
+
+	# cmd
+	sbatch.append(cmd)
+
+	return sbatch
+
+def _sbatchstr(name, cmd, hours, partition=None):
+	sbatch=[]
+	sbatch.append('#!/bin/bash')
+	sbatch.append('#SBATCH --nodes=1')
+	sbatch.append('#SBATCH --cpus-per-task=8')
+	sbatch.append('#SBATCH --mem=128G')
 	
 	sbatch.append('\n#SBATCH --time=%d:0:0\n' % hours)
 	print('%s %d' % (name, hours))
@@ -28,7 +51,7 @@ def _sbatchstr(name, cmd, hours, partition):
 
 	return sbatch
 
-def sbatchfiles(args):
+def py(args):
 	assert len(args)==2, 'Usage: python sbatch2.py sbatchfiles job.stub hours'
 	hours = int(args[1])
 	partition = args[2] if len(args) == 3 else None
@@ -41,7 +64,20 @@ def sbatchfiles(args):
 		with open(outfile, 'w') as fout:
 			fout.write('%s\n\n' % ('\n'.join(sbatchstr)))
 		cp._info('save to %s\n' % outfile)
-	
+
+def af2(args):
+	assert len(args)==2, 'Usage: python sbatch2.py af2 job.stub hours'
+	hours = int(args[1])
+	partition = args[2] if len(args) == 3 else None
+
+	for j in cp.loadtuples(args[0], delimiter=','): # name, cmd
+		name = j[0]
+		cmd = j[1]
+		sbatchstr = _af2str(name, cmd, hours)
+		outfile = '%s.sh' % name
+		with open(outfile, 'w') as fout:
+			fout.write('%s\n\n' % ('\n'.join(sbatchstr)))
+		cp._info('save to %s\n' % outfile)
 
 if __name__=='__main__':
 	cp.dispatch(__name__)
