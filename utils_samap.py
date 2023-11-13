aac65393cc5580a2249bc3fd32b0c1842227038d
Kejue Jia <jkjium@gmail.com>
2023-11-13 00:57:32 -0500

    _filter_samat

--- utils_samap.py
+++ utils_samap.py
@@ -9,8 +9,9 @@ from itertools import groupby
 # c1,c2 column pair indicating x,y ids
 # c_value: homology weight
 # reciprocate: keep only bi-directional homology 
-def _flat2mat(nptable, c1, c2, c_value, delimiter=',', reciprocate=True):
-    # get all labels (genes)
+# return scipy sparse matrix: gnnm, labels: gns
+def _flat2spmat(nptable, c1, c2, c_value, delimiter=',', reciprocate=True):
+    # get ordered unique labels (genes)
     labels = np.sort(np.unique(np.concatenate((nptable[:,c1], nptable[:,c2]))))
     # convert genes to integers
     ix = labels.searchsorted(nptable[:,c1])
@@ -26,12 +27,33 @@ def _flat2mat(nptable, c1, c2, c_value, delimiter=',', reciprocate=True):
     gnnm_obj = {'gnnm': gnnms, 'gns':labels}
     return gnnm_obj
 
+# thresholding scipy sparse matrix 
+# gnnm: scipy.sparse.coo_matrix; thr = scaler
+# return: symmetrical filtered gnnm
+def _filter_spmat(gnnm, thr=0.25):
+	# threshold by row max
+    thresholds = gnnm.max(axis=1).A.flatten() * thr
+    x, _ = gnnm.nonzero()
+    gnnm.data[gnnm.data < thresholds[x]] = 0 
+    gnnm.eliminate_zeros()
+	
+	# make symmetrical
+    x,y = gnnm.nonzero()
+    z = gnnm.data
+    gnnm = gnnm.tolil()
+    gnnm[y,x] = z
+    gnnm.tocsr()
+    return gnnm    
+
 
 def foo(args):
-    # all_maps.txt
+    pass
+
+def ut_flat2spmat(args):
+    # unit_test/all_maps.txt
     nptable = np.loadtxt(args[0],dtype='str')
 
-    gnnm_obj = _flat2mat(nptable, 0,1,11,' ')
+    gnnm_obj = _flat2spmat(nptable, 0,1,11,' ')
     gns = gnnm_obj['gns']
     gns_dict = dict((s[0],list(s[1])) for s in groupby(gns, lambda x: x.split('.')[0]))
     gnnms = gnnm_obj['gnnm']
